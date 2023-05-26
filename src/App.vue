35d027b14cea15b14a3a2e8dcd93b2bee30dca3e
OneSummerDay <artem.kraievyi@gmail.com>
2023-05-26 21:56:56 +0300

    created the header component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { CheckCircleIcon} from '@heroicons/vue/24/solid'
+
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+import TheHeader from './components/TheHeader.vue'
 
 const navItems = {
   timeline: ClockIcon,
@@ -11,36 +12,11 @@ const navItems = {
 
 <template>
 <!--Header-->
-  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
-    <a href="#">
-      <img src="./assets/logo.png" alt="Logo" class="h-9">
-    </a>
-    <a href="#" class="text-sm">
-      <div v-if="true" class="flex it-center gap-1">
-        Day complete!
-        <CheckCircleIcon class="h-7 text-green-500" />
-      </div>
-      <div v-else class="flex it-center gap-1">
-        <div>Progress: <span class="font-mono">20%</span></div>
-        <div class="h-3 w-3 rounded-full bg-red-500"></div>
-      </div>
-    </a>
-  </header>
+  <TheHeader />
 <!--Main block-->
   <main class="flex flex-grow flex-col"></main>
 
 <!--Navigation menu-->
-  <nav class="sticky bottom-0 z-10 bg-white">
-
-    <ul class="flex items-center justify-around border-t">
-
-      <li v-for="icon, page in navItems" :key="page" class="flex-1">
-        <a :href="`#${ page }`" class="flex flex-col items-center p-2 text-xs capitalize" >
-          <component :is="icon" class="h-6 w-6" /> {{ page }}
-        </a>
-      </li>
-      
-    </ul>
-  </nav>
+  
 </template>
 
