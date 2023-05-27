576e4e8fb999a5c44ba3c49d2b9a29de8f37040c
OneSummerDay <artem.kraievyi@gmail.com>
2023-05-27 20:42:57 +0300

    added props

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,27 +1,14 @@
 <script setup>
-import { ref } from 'vue'
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from '../constants'
 import NavItem from './NavItem.vue'
+
+defineOptions(['currentPage'])
 
 const navItems = {
   [PAGE_TIMELINE]: ClockIcon,
   [PAGE_ACTIVITIES]: ListBulletIcon,
   [PAGE_PROGRESS]: ChartBarIcon
-}
-
-const currentPage = ref(normalizePageHash())
-
-function normalizePageHash() {
-    const hash = window.location.hash.slice(1)
-
-    if (Object.keys(navItems).includes(hash)){
-        return hash
-    }
-
-    window.location.hash = PAGE_TIMELINE
-
-    return PAGE_TIMELINE
 }
 </script>
 
