3f0b5ce1dbc0296cc50ce5ae5a9bfcda314a95a3
OneSummerDay <artem.kraievyi@gmail.com>
2023-05-28 16:01:56 +0300

    added clickable element in header

--- src/App.vue
+++ src/App.vue
@@ -20,11 +20,18 @@ function normalizePageHash() {
 
     return PAGE_TIMELINE
 }
+
+function goTo(page){
+  currentPage.value = page
+}
 </script>
 
 <template>
 
-  <TheHeader />
+  <TheHeader 
+  @go-to-timeline="goTo(PAGE_TIMELINE)" 
+  @go-to-progress="goTo(PAGE_PROGRESS)"
+  />
 
   <main class="flex flex-grow flex-col">
     <TheTimeline v-show="currentPage === PAGE_TIMELINE" />
@@ -32,7 +39,7 @@ function normalizePageHash() {
     <TheProgress v-show="currentPage === PAGE_PROGRESS" />
   </main>
 
-  <TheNav :current-page="currentPage" @navigate="currentPage = $event" />
+  <TheNav :current-page="currentPage" @navigate="goTo($event)" />
     
 </template>
 
